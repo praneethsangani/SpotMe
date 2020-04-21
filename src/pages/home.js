fb93cc0e25de55293d33fb370c1389fb58f6ff28
woodpjames <47159440+woodpjames@users.noreply.github.com>
2020-04-21 17:40:43 -0400

    change homw

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,11 +7,22 @@ import Cards from '../components/cards/Cards';
 
 import { connect } from 'react-redux';
 
+
+
 class home extends Component {
   componentDidMount() {
   }
   render() {
-    return (
+    var isVegetarian = false;
+    const {
+      classes,
+      user: {
+          credentials: {name, createdAt, imageUrl, bio, phoneNumber, gym},
+          authenticated
+      }
+     } = this.props;
+    return authenticated ? 
+    (
       <Grid container spacing={16}>
         <Grid item sm={8} xs={12}>
           <Cards/>
@@ -20,7 +31,13 @@ class home extends Component {
           <Profile />
         </Grid>
       </Grid>
-    );
+    ) : (
+      <Grid container spacing={16}>
+        <Grid item sm={4} xs={12}>
+          <Profile />
+        </Grid>
+      </Grid>
+    )
   }
 }
 
